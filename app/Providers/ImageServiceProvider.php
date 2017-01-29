45418b59faff4d7699119967c796a88607222dca
Mike Lovely <mike-lovely@hotmail.co.uk>
2017-01-29 14:14:51 +0000

    Initial commit

---
+++ app/Providers/ImageServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Providers;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Intervention\Image\ImageManager;
+
+class ImageServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $app)
+    {
+        $app['image'] = $app->factory(function() {
+            return new ImageManager;
+        });
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app->get('/{width}/{height}', function (Request $request, Silex\Application $app, $width, $height) {
+
+    $clause = $request->get('image') ? "WHERE id = ?" : "ORDER BY rand() LIMIT 1";
+
+    $image = $app['db']->fetchAssoc("SELECT filename FROM images {$clause}", [$request->get('image')]);
+    
+    $placeholder = $app['cache']->fetch($cacheKey = "{$width}:{$height}:{$request->get('image')}");
+    
+    if ($placeholder === false) {
+        $placeholder = $app['image']
+            ->make(__DIR__ . '/../public/img/' . $image['filename'])
+            ->fit($width, $height)
+            ->greyscale()
+            ->response('png');
+
+        $app['cache']->store($cacheKey, $placeholder);
+    }
+
+    return new Response($placeholder, 200, [
+        'Content-Type' => 'image/png',
+    ]);
+})->assert('width', '[0-9]+')->assert('height', '[0-9]+');
